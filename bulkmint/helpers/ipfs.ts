1f6603a0d6f3ca02021f22d96e6e6dcb5d0bc9db
0xminer11 <arali.naveen1@gmail.com>
2024-08-22 10:07:14 +0530

    feat: add bulk mint script to make transactions from smart account

---
+++ bulkmint/helpers/ipfs.ts
@@ -0,0 +1,75 @@
+import {create} from "ipfs-http-client";
+import axios from "axios";
+
+// const PROJECT_ID = '2MzXNxeCq2tIsubEa48lHX76EFM';
+// const PROJECT_SECRET = 'ddb45fd87ba4d4e9f37dfbb4f189fd31';
+
+// console.log("ProjectId",PROJECT_ID);
+// console.log("ProjectSecrate",PROJECT_SECRET);
+
+// const client = create({
+//   host: "ipfs.infura.io",
+//   port: 5001,
+//   protocol: "https",
+//   headers: {
+//     authorization: `Basic ${Buffer.from(`${PROJECT_ID}:${PROJECT_SECRET}`).toString("base64")}`,
+//   },
+// });
+
+
+
+
+
+
+
+// console.log('IPFS client created', client);
+
+
+export const uploadFile = async (file:any) => {
+  try {
+    let data = JSON.stringify({
+      "file": file
+    });
+
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://nftrace-blockchain-dev.chaincodeconsulting.com/certificates/add-file-to-ipfs',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    const result = await axios.request(config);
+    return JSON.stringify(result.data.Hash);
+  } catch (e) {
+    console.log('Error while uploading file to IPFS', e);
+    console.error(e); // Log the error message
+  }
+};
+ 
+uploadFile("hello")
+
+// export const addFile = async (file) =>{
+// try{
+// let data = JSON.stringify({
+//   "file": file
+// });
+
+// let config = {
+//   method: 'post',
+//   maxBodyLength: Infinity,
+//   url: 'http://127.0.0.1:3001/certificates/addFile',
+//   headers: { 
+//     'Content-Type': 'application/json'
+//   },
+//   data : data
+// };
+
+// const res = await axios.request(config);
+// console.log("Result",res);
+// return res;
+//   } catch(error){
+//     console.log("Error : Error in adding file",error);
+//   }
+// }
---
+++ bulkmint/index.ts
@@ -0,0 +1,296 @@
+import {
+    encodeFunctionData,
+    parseAbi,
+    createWalletClient,
+    http,
+    createPublicClient,
+  } from "viem";
+
+import * as fs from 'fs';
+import mysql from 'mysql';
+import util from 'util';
+import web3 from 'web3';
+import crypto from 'crypto';
+import {abi} from './helpers/abi/factory.json'
+
+import { getImage, uploadVerifiableFile } from './helpers/gcp';
+import { uploadFile } from './helpers/ipfs';
+import { privateKeyToAccount } from "viem/accounts";
+import { polygonAmoy } from "viem/chains";
+import { createSmartAccountClient } from "@biconomy/account";
+import { PaymasterMode } from "../src";
+import { ethers } from "ethers";
+// import {connectContract} from './helpers/connectContract';
+
+
+
+const GCP_NFTTRACE_BUCKET_NAME_IMAGE = "nfttrace-cert-image-live";
+const FACTORY_CONTRACT_ADDRESS='0x915BB62190F3FB84400408FBc7F6938477db30F2';
+let privateKey = process.env.PRIVATE_KEY || '';
+let wallet;
+
+const RPC_URL = process.env.RPC_URL? process.env.RPC_URL : 'https://polygon-mumbai.infura.io/v3/7b2';
+const web3provider = new web3(new web3.providers.HttpProvider(RPC_URL));
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+pool.query = util.promisify(pool.query);
+
+const queryIsProcessedNftAsc = async() => {
+    try {
+        const results = await pool.query(
+            'SELECT * FROM bulk_cert_requests WHERE is_processed = 0 ORDER BY id ASC LIMIT 1'
+        );
+        return results;
+    } catch (error) {
+        console.log("Error: ", error);     
+        throw error; 
+    }
+};
+
+// const addTxtoPool = async(to,encodedData) => {
+//     try {
+//         const results = await pool.query(
+//             'INSERT INTO tx_pool (to_address,encoded_data) VALUES (?,?)',
+//             [to,encodedData]
+//         );
+//         return results;
+//     } catch (error) {
+//         console.log("Error: ", error);     
+//         throw error; 
+//     }
+// }
+
+const updateNftStatus = async(token_id) => {
+    try {
+        const results = await pool.query(
+            'UPDATE bulk_cert_requests SET is_processed = 1 WHERE token_id = ?',
+            [token_id]
+        );
+        return results;
+    } catch (error) {
+        console.log("Error: ", error);     
+        throw error; 
+    }
+}
+
+
+const fetchContractAddress = async(categoryid) => {
+    try {
+        const results = await pool.query(
+            'SELECT contract FROM nft_document_categories WHERE id = ?',
+            [categoryid]
+        );
+        return results[0].contract;
+    } catch (error) {
+        console.log("Error: ", error);     
+        throw error; 
+    }
+}
+
+export const connectContract = async (contractAddress:string) => {
+    try {
+      const contract = new ethers.Contract(contractAddress,abi);
+      return contract;
+    } catch (e) {
+      console.log('Error while connecting contract', e);
+    }
+}
+
+const ProcessNFTMetadata = async(data) => {
+    try {
+        const contractAddressSet = new Set<string>();
+        let params = {};
+
+        const mintTxs: { to: string; data: string }[] = [];
+        for (let i = 0; i < data.length; i++) {
+
+
+            console.log("processing NFT of id: ", data[i].token_id);
+            console.log("Fetching NFT Image from GCP for token_id: ", data[i].token_id);
+
+            const Image_Path = `${data[i].job_id}/${data[i].token_id}.jpg`;
+            const Image = await getImage(Image_Path) || '';
+            const ImageBuffer = Buffer.from(Image);
+            const ImageIPFS = await uploadFile(ImageBuffer);
+            console.log("Image Uploaded to IPFS: ", ImageIPFS);
+            let name = data[i].name;
+            let description =data[i].description;
+            let category =data[i].category;
+            let recipient =data[i].recipient;
+            let imageURI = `https://ipfs.io/ipfs/${ImageIPFS}`;
+            let tokenId = data[i].token_id;
+            let metaData = JSON.parse(data[i].metadata);
+            let keys = Object.keys(metaData);
+
+            var attributes : any = [];
+            for (var j = 0; j < keys.length; j++) {
+              var trait_type = keys[j];
+              var value = JSON.stringify(metaData[trait_type]).replace(/"/g, '');
+              let trait = JSON.stringify({ "trait_type": trait_type, "value": value })
+              attributes.push(JSON.parse(trait));
+            }
+
+            const odHashForImage = crypto.createHash('sha3-256').update(Image).digest('hex');
+            const messageHash = await web3provider.utils.sha3(odHashForImage) || '';
+            let signature = (await web3provider.eth.accounts.sign(messageHash, `0x${privateKey}`)).signature;
+            let signtrait = JSON.stringify({ "trait_type":   "signature", "value": signature })
+            let odhashtrait = JSON.stringify({ "trait_type": "ODHash",  "value": odHashForImage })
+            attributes.push(JSON.parse(signtrait));
+            attributes.push(JSON.parse(odhashtrait));
+        let uploadJson = {
+            name,
+            description,
+            category,
+            image: `https://storage.cloud.google.com/${GCP_NFTTRACE_BUCKET_NAME_IMAGE}/${Image_Path}`,
+            attributes: attributes,
+        }; 
+        
+        const metadatahash = await uploadFile(JSON.stringify(uploadJson));
+        console.log("Metadata Uploaded to IPFS: ", metadatahash);
+
+        // const metadatahash = 'metaDataHash';
+
+        const contract_address = await fetchContractAddress(data[i].category_id);
+        const factoryContract = await connectContract(FACTORY_CONTRACT_ADDRESS);
+        
+        const encodedData = factoryContract?.interface.encodeFunctionData(
+            'issueCertificate',
+            [contract_address, recipient, tokenId, imageURI]
+        );
+        
+        if (!contractAddressSet.has(contract_address)) {
+            contractAddressSet.add(contract_address);
+            params[contract_address] = {
+                address: [],
+                tokenId: [],
+                tokenURI: [],
+                odHash: [],
+                imageBuffer: [],
+                imageName: [],
+                attributes: [],
+                mintTx: [],
+            };
+        }
+
+        params[contract_address].address.push(recipient);
+        params[contract_address].tokenId.push(tokenId);
+        params[contract_address].tokenURI.push(metadatahash);
+        params[contract_address].odHash.push(odHashForImage);
+        params[contract_address].imageBuffer.push(Buffer.from(Image));
+        params[contract_address].imageName.push(`${tokenId}.jpg`);
+        params[contract_address].attributes.push(attributes);  
+        params[contract_address].mintTx.push({ to: FACTORY_CONTRACT_ADDRESS, data: encodedData }); 
+    }
+
+  
+
+    console.log("Embedding Metadata for NFTs:::::::::::::::: ");
+    console.log("Contract Address Set: ", contractAddressSet);
+
+    for (const nftContract of contractAddressSet){
+
+        console.log("Embedding Metadata for Contract: ", nftContract);
+        
+        const {transactionHash} = await processTxs(params[nftContract].mintTx);
+        
+        for(let i=0;i<params[nftContract].tokenId.length;i++) {
+
+        console.log("Embedding Metadata for tokenId: ", params[nftContract].tokenId[i]);
+        
+            let embedData = {
+                "tokenId": params[nftContract].tokenId[i],
+                "issuer": wallet,
+                "recipient": params[nftContract].address[i],
+                "txHash": transactionHash,
+                "ODHash": params[nftContract].odHash[i],
+                "metaDataHash": params[nftContract].tokenURI[i],
+                "blockchain": "polygon",
+            };
+
+        const embededMetaData = `<--NFTRACE_KEY_START-->${Buffer.from(JSON.stringify(embedData))}<--NFTRACE_KEY_END-->`;       
+        const modifiedFile = Buffer.concat([params[nftContract].imageBuffer[i], Buffer.from(embededMetaData)]);
+        await uploadVerifiableFile(modifiedFile, params[nftContract].imageName[i]);
+    }
+    console.log("Embedding Metadata for NFTs Completed:::::::::::::::: ");
+    console.log("Updating NFT Status in DB:::::::::::::::: ");
+    for(let i=0;i<params[nftContract].tokenId.length;i++) {
+        await updateNftStatus(params[nftContract].tokenId[i]);
+    }
+    console.log(":::::::::::::::: NFT Status Updated in DB :::::::::::::::: ");
+}
+
+
+
+    } catch (error) {
+        console.log("Error: ", error);     
+        throw error; 
+    }
+}
+
+const processTxs = async(mintTxs) => {
+    const paymasterUrl = "https://paymaster.biconomy.io/api/v1/80002/qX9uXTHNm.dc1e610e-daee-46a8-acb2-7b6438fd8428"
+    const bundlerUrl = "https://bundler.biconomy.io/api/v2/80002/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44";
+    
+    const account = privateKeyToAccount(`0x${privateKey}`);
+    const signer = createWalletClient({ account, chain: polygonAmoy, transport: http() });
+    const smartAccount = await createSmartAccountClient({
+        signer,
+        bundlerUrl,
+        paymasterUrl,
+      }); // Retrieve bundler and pymaster urls from dashboard
+    const getAddress = await smartAccount.getAddress();
+
+    console.log("SmartAccount :", getAddress);
+    
+
+    const buildUseropDto = {
+        paymasterServiceData: {
+        mode: PaymasterMode.ERC20,
+    },
+    };
+console.log("========= Sending Trasaction to blockchain ======");
+
+const { wait } = await smartAccount.sendTransaction(
+    mintTxs,
+    buildUseropDto
+);
+
+  const {
+    receipt: { transactionHash },
+    userOpHash,
+    success,
+  } = await wait();
+console.log("Transaction hash :",transactionHash);
+
+  return { transactionHash, userOpHash, success };
+}
+
+
+
+
+
+
+async function main() {
+
+    try {
+        const getTxs = await queryIsProcessedNftAsc();
+        if (getTxs.length > 0) {
+            await ProcessNFTMetadata(getTxs);
+        } else {
+            console.log("No NFTs to process....");
+            }
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+}
+
+
+main();
+
